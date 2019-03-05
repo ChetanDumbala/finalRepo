5d15be4f2b666b658ea4e39a5ae20f6c2c4ad7c9
ChetanDumbala <41179360+ChetanDumbala@users.noreply.github.com>
2019-03-05 12:01:01 +0530

    test

---
+++ testing12/Controllers/Controller12.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testing12.Controllers
+{
+    public class Controller12 : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+    }
+}
